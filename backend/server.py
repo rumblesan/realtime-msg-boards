5e6663773dd15c89083d970feb639152e4c40d0a
Guy John <guy@rumblesan.com>
2014-10-20 11:54:57 +0100

    Adding endpoint to backend

--- backend/server.py
+++ backend/server.py
@@ -4,9 +4,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello, World!"
+    return 'Hello, World!'
 
-if __name__ == "__main__":
+
+@app.route('/word/<newword>', methods=['POST'])
+def newWord(word):
+    print(word)
+
+
+if __name__ == '__main__':
     app.run()
