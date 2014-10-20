89c19357f236dbff7ce034798bc34007e61fad73
Guy John <guy@rumblesan.com>
2014-10-20 15:40:20 +0100

    General cleanup

--- static/js/app.js
+++ static/js/app.js
@@ -1,70 +1,89 @@
 /*jslint browser: true */
-/*global word */
+/*global word, reqwest */
 
 (function (w) {
 
-    var app, appstate, constants;
+    w.createApp = function (cfg, urls) {
 
-    app = {};
-    appstate = {
-        words: [],
-        grabbedWord: null
-    };
-    constants = {
-        textSize: 30,
-        canvasHeight: 768,
-        canvasWidth: 1024
-    };
+        var App, appstate, constants;
 
-    app.processing = function (processing) {
-
-        processing.setup = function () {
-            processing.size(constants.canvasWidth, constants.canvasHeight);
-            processing.background(255);
-
-            processing.textSize(constants.textSize);
-
-
+        App = {};
+        appstate = {
+            words: [],
+            grabbedWord: null
+        };
+        constants = {
+            textSize: 30,
+            canvasHeight: 768,
+            canvasWidth: 1024
         };
 
-        processing.draw = function () {
-            processing.background(200);
-            var i;
-            for (i = 0; i < appstate.words.length; i += 1) {
-                appstate.words[i].draw(processing);
-            }
-        };
+        App.processing = function (processing) {
 
-        processing.mousePressed = function () {
-            var w, i;
-            for (i = 0; i < appstate.words.length; i += 1) {
-                w = appstate.words[i];
-                if (w.inBounds(processing.mouseX, processing.mouseY)) {
-                    appstate.grabbedWord = w;
+            processing.setup = function () {
+                processing.size(constants.canvasWidth, constants.canvasHeight);
+                processing.background(255);
+
+                processing.textSize(constants.textSize);
+            };
+
+            processing.draw = function () {
+                processing.background(200);
+                var i;
+                for (i = 0; i < appstate.words.length; i += 1) {
+                    appstate.words[i].draw(processing);
                 }
-            }
+            };
+
+            processing.mousePressed = function () {
+                var w, i;
+                for (i = 0; i < appstate.words.length; i += 1) {
+                    w = appstate.words[i];
+                    if (w.inBounds(processing.mouseX, processing.mouseY)) {
+                        appstate.grabbedWord = w;
+                    }
+                }
+            };
+
+            processing.mouseReleased = function () {
+                appstate.grabbedWord = null;
+            };
+
+            processing.mouseDragged = function () {
+                if (appstate.grabbedWord !== null) {
+                    appstate.grabbedWord.setPosition(processing.mouseX, processing.mouseY);
+                }
+            };
+
         };
 
-        processing.mouseReleased = function () {
-            appstate.grabbedWord = null;
+        App.addWord = function (newWord, xPos, yPos) {
+            appstate.words.push(word.create(newWord, xPos, yPos, constants.textSize));
         };
 
-        processing.mouseDragged = function () {
-            if (appstate.grabbedWord !== null) {
-                appstate.grabbedWord.setPosition(processing.mouseX, processing.mouseY);
-            }
+        App.createWord = function (newWord) {
+            var xPos, yPos;
+            xPos = Math.random() * (constants.canvasWidth - 100);
+            yPos = Math.random() * (constants.canvasHeight - 30);
+            reqwest({
+                url: urls.createword,
+                method: 'post',
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    word: newWord,
+                    xPos: xPos,
+                    yPos: yPos
+                }),
+                success: function (resp) {
+                    console.log(resp);
+                }
+            });
+
         };
+
+        return App;
 
     };
-
-    app.createWord = function (newWord) {
-        var xPos, yPos;
-        xPos = Math.random() * (constants.canvasWidth - 100);
-        yPos = Math.random() * (constants.canvasHeight - 30);
-        appstate.words.push(word.create(newWord, xPos, yPos, constants.textSize));
-    };
-
-    w.app = app;
 
 }(window));
 
