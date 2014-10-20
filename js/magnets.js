8b2414b5034688571ac3c067b1e62ba2d7d4456e
Guy John <guy@rumblesan.com>
2014-10-20 11:31:48 +0100

    Moving words

--- js/magnets.js
+++ js/magnets.js
@@ -3,25 +3,49 @@
 
 var magnets = function (processing) {
 
-    var appstate = {
-        words: []
+    var appstate, constants;
+    appstate = {
+        words: [],
+        grabbedWord: null
+    };
+    constants = {
+        textSize: 30
     };
 
     processing.setup = function () {
         processing.size(1024, 768);
-        processing.background(100);
+        processing.background(255);
 
-        processing.textSize(30);
+        processing.textSize(constants.textSize);
 
-        appstate.words.push(word.create('foo', 300, 300));
+        appstate.words.push(word.create('testing', 300, 300, constants.textSize));
 
     };
 
     processing.draw = function () {
+        processing.background(200);
         var i;
         for (i = 0; i < appstate.words.length; i += 1) {
             appstate.words[i].draw(processing);
         }
+    };
+
+    processing.mousePressed = function () {
+        var w, i;
+        for (i = 0; i < appstate.words.length; i += 1) {
+            w = appstate.words[i];
+            if (w.inBounds(processing.mouseX, processing.mouseY)) {
+                appstate.grabbedWord = w;
+            }
+        }
+    };
+
+    processing.mouseReleased = function () {
+        appstate.grabbedWord = null;
+    };
+
+    processing.mouseDragged = function () {
+        appstate.grabbedWord.setPosition(processing.mouseX, processing.mouseY);
     };
 
 };
--- js/word.js
+++ js/word.js
@@ -5,14 +5,18 @@
 
     var word = {};
 
-    word.create = function (text, startXPos, startYPos) {
+    word.create = function (text, startXPos, startYPos, textSize) {
 
-        var word = {},
-            wordstate = {
-                text: text,
-                xPos: startXPos,
-                yPos: startYPos
-            };
+        var word, wordstate;
+
+        word = {};
+        wordstate = {
+            text: text,
+            xPos: startXPos,
+            yPos: startYPos,
+            xSize: (text.length * 15),
+            ySize: textSize + 5
+        };
 
         word.getText = function () {
             return wordstate.text;
@@ -28,7 +32,32 @@
         };
 
         word.draw = function (processing) {
-            processing.text(wordstate.text, wordstate.xPos, wordstate.yPos);
+            var x1, x2, y1, y2;
+            x1 = wordstate.xPos;
+            x2 = wordstate.xPos + wordstate.xSize;
+            y1 = wordstate.yPos;
+            y2 = wordstate.yPos + wordstate.ySize;
+
+            processing.beginShape();
+            processing.vertex(x1, y1);
+            processing.vertex(x2, y1);
+            processing.vertex(x2, y2);
+            processing.vertex(x1, y2);
+            processing.vertex(x1, y1);
+            processing.endShape();
+            processing.fill(0);
+            processing.text(wordstate.text, (x1 + 3), (y1 + 3), wordstate.xSize, wordstate.ySize);
+            processing.fill(255);
+        };
+
+        word.inBounds = function (xCoord, yCoord) {
+            var x1, x2, y1, y2;
+            x1 = wordstate.xPos;
+            x2 = wordstate.xPos + wordstate.xSize;
+            y1 = wordstate.yPos;
+            y2 = wordstate.yPos + wordstate.ySize;
+
+            return ((xCoord >= x1) && (xCoord <= x2) && (yCoord >= y1) && (yCoord <= y2));
         };
 
         return word;
